804a572f5ca51a30892c369b620646e09d6cb352
kykougaku <6489550+kykougaku@users.noreply.github.com>
2024-07-10 15:13:38 +0900

    change updata.bat     add tooltip and change repo user from plusf to chiashilab

--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 git pull origin main
 CALL venv\Scripts\activate
 python -m pip uninstall -y calibrator dataloader
-python -m pip install git+https://github.com/PlusF/Calibrator git+https://github.com/PlusF/DataLoader
+python -m pip install git+https://github.com/chiashi-lab/Calibrator git+https://github.com/chiashi-lab/DataLoader git+https://github.com/chiashi-lab/Tooltip
